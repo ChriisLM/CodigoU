da2b54e413cd90178fcd0fcebd31b25e6d948650
ChriisLM <christianlezcano878@gmail.com>
2024-12-06 17:13:01 -0500

    feat: :sparkles: Add route simulator

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import { SimuladorCicuitos } from './pages/SimuladorCircuitos.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}/>
-        <Route path='' element={<App />}/>
+        <Route path='/SimuladorCircuitos' element={<SimuladorCicuitos />}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
