683449afee962bf564f533d585953b4b5c4134ab
ChriisLM <christianlezcano878@gmail.com>
2024-12-08 16:01:56 -0500

    feat: :sparkles: Add new functionality to add components

--- src/components/SimuladorCircuitos/Board.tsx
+++ src/components/SimuladorCircuitos/Board.tsx
@@ -1,15 +1,45 @@
+import { useRef, useState } from "react";
 import "../../assets/css/SimuladorCircuitos/Board.css"
 import { SideBar } from "./Sidebar";
+import { ElectronicComponent } from "./ElectronicComponent";
 
+interface Component{
+  type: string;
+  Component: React.ElementType;
+}
 
+const initialComponents: Component[] = [];
 export function Board () {
+  const [components,setComponents] = useState<Component[]>(initialComponents)
+  const countersRef = useRef<Record<string, number>>({});
+
+  const handleComponentsBoard = ({ type, Component }: Component) => {
+    if (!countersRef.current[type]) {
+      countersRef.current[type] = 1;
+    }
+
+    const componentData = {
+      id: `${type}${countersRef.current[type]}`,
+      type,
+      Component,
+    };
+
+    countersRef.current[type]++;
+    setComponents((prev) => [...prev, componentData]);
+  };
+
   return (
     <section className="board">
-      <div className="board__rule"></div>
       <div className="board__container">
-        <SideBar/>
+        {components.map((component,index) => {
+          return (
+            <ElectronicComponent key={index} Component={component.Component} inBoard={true}/>
+          )
+        })}
       </div>
-      <aside className="board__aside"></aside>
+      <aside className="board__aside">
+        <SideBar addComponents={handleComponentsBoard}/>
+      </aside>
     </section>
   );
 };
--- src/components/SimuladorCircuitos/ElectronicComponent.tsx
+++ src/components/SimuladorCircuitos/ElectronicComponent.tsx
@@ -1,13 +1,14 @@
-// import { DCPower, Resistor } from "../../utils/IconsElectronic";
 import "../../assets/css/SimuladorCircuitos/ElectronicComponent.css"
 
 interface Props {
   Component: React.ElementType;
+  onclick?: React.FunctionComponent
+  inBoard: boolean
 }
 
-export function ElectronicComponent({Component}: Props) {
+export function ElectronicComponent({Component,onclick, inBoard}: Props) {
   return (
-    <div className="electronic-component">
+    <div onClick={onclick} className={`electronic-component ${inBoard ? "electronic-component--board" : ""}`}>
       <span className="conector__line conector__line--left"></span>
       <Component/>
       <span className="conector__line conector__line--right"></span>
