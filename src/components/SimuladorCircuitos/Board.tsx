bb619ef32e38ce5a5112fe1e647a17f8e4aeb7bb
ChriisLM <christianlezcano878@gmail.com>
2024-12-09 17:51:21 -0500

    feat: :recycle: Add new properties to component in board

--- src/components/SimuladorCircuitos/Board.tsx
+++ src/components/SimuladorCircuitos/Board.tsx
@@ -1,45 +1,51 @@
-import { useRef, useState } from "react";
-import "../../assets/css/SimuladorCircuitos/Board.css"
+import { useState, useRef } from "react";
+import "../../assets/css/SimuladorCircuitos/Board.css";
 import { SideBar } from "./Sidebar";
 import { ElectronicComponent } from "./ElectronicComponent";
 
-interface Component{
+interface Component {
+  id: string;
   type: string;
   Component: React.ElementType;
+  x: number;
+  y: number;
 }
 
 const initialComponents: Component[] = [];
-export function Board () {
-  const [components,setComponents] = useState<Component[]>(initialComponents)
+
+export function Board() {
+  const [components, setComponents] = useState<Component[]>(initialComponents);
   const countersRef = useRef<Record<string, number>>({});
 
-  const handleComponentsBoard = ({ type, Component }: Component) => {
+  const handleAddComponent = (type: string, Component: React.ElementType) => {
     if (!countersRef.current[type]) {
       countersRef.current[type] = 1;
     }
 
-    const componentData = {
-      id: `${type}${countersRef.current[type]}`,
+    const newComponent: Component = {
+      id: `${type}-${countersRef.current[type]}`,
       type,
       Component,
+      x: 0,
+      y: 0,
     };
 
     countersRef.current[type]++;
-    setComponents((prev) => [...prev, componentData]);
+    setComponents((prev) => [...prev, newComponent]);
   };
 
   return (
     <section className="board">
-      <div className="board__container">
+        <div className="board__container">
         {components.map((component,index) => {
           return (
             <ElectronicComponent key={index} Component={component.Component} inBoard={true}/>
           )
         })}
-      </div>
-      <aside className="board__aside">
-        <SideBar addComponents={handleComponentsBoard}/>
-      </aside>
+        </div>
+        <aside className="board__aside">
+        <SideBar addComponents={handleAddComponent}/>
+        </aside>
     </section>
   );
-};
+}
