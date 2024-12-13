b8cb11af640ac2f808f9931f0a98f5e7662236e9
ChriisLM <christianlezcano878@gmail.com>
2024-12-12 21:37:49 -0500

    feat: :construction: Change connections of components

--- src/components/SimuladorCircuitos/ElectronicComponent.tsx
+++ src/components/SimuladorCircuitos/ElectronicComponent.tsx
@@ -4,11 +4,12 @@ interface Props {
   Component: React.ElementType;
   onclick?: React.FunctionComponent
   inBoard: boolean
+  style?: any
 }
 
-export function ElectronicComponent({Component,onclick, inBoard}: Props) {
+export function ElectronicComponent({Component,onclick, inBoard, style}: Props) {
   return (
-    <div onClick={onclick} className={`electronic-component ${inBoard ? "electronic-component--board" : ""}`}>
+    <div onClick={onclick} style={style} className={`electronic-component ${inBoard ? "electronic-component--board" : ""}`}>
       <span className="conector__line conector__line--left"></span>
       <Component/>
       <span className="conector__line conector__line--right"></span>
