cb0bb88803cb26ef4fb9bb75683c093e61b08a68
ChriisLM <christianlezcano878@gmail.com>
2024-12-14 20:47:16 -0500

    feat: :construction: Add functionality of conector lines

--- src/components/SimuladorCircuitos/ElectronicComponent.tsx
+++ src/components/SimuladorCircuitos/ElectronicComponent.tsx
@@ -1,18 +1,60 @@
-import "../../assets/css/SimuladorCircuitos/ElectronicComponent.css"
+import "../../assets/css/SimuladorCircuitos/ElectronicComponent.css";
 
 interface Props {
   Component: React.ElementType;
-  onclick?: React.FunctionComponent
-  inBoard: boolean
-  style?: any
+  onclick?: () => void;
+  inBoard: boolean;
+  style?: React.CSSProperties;
+  id?: string;
+  onConnectorClick?: (data: {
+    id: string;
+    positionX: number;
+    positionY: number;
+  }) => void;
 }
 
-export function ElectronicComponent({Component,onclick, inBoard, style}: Props) {
+export function ElectronicComponent({
+  Component,
+  onclick,
+  inBoard,
+  style,
+  id = "",
+  onConnectorClick,
+}: Props) {
+  const handleConnectorClick = (e: React.MouseEvent<HTMLSpanElement>) => {
+    if (onConnectorClick) {
+      // Obtener las coordenadas del conector
+      const rect = e.currentTarget.getBoundingClientRect();
+      const positionX = rect.x + rect.width / 2; // Centro del conector en X
+      const positionY = rect.y + rect.height / 2; // Centro del conector en Y
+      console.log("aqui llegue conecttor", positionX, positionY);
+      
+      // Pasar los datos al callback
+      onConnectorClick({
+        id,
+        positionX,
+        positionY,
+      });
+    }
+  };
+
   return (
-    <div onClick={onclick} style={style} className={`electronic-component ${inBoard ? "electronic-component--board" : ""}`}>
-      <span className="conector__line conector__line--left"></span>
-      <Component/>
-      <span className="conector__line conector__line--right"></span>
+    <div
+      onClick={onclick}
+      style={style}
+      className={`electronic-component ${
+        inBoard ? "electronic-component--board" : ""
+      }`}
+    >
+      <span
+        className="conector__line conector__line--left"
+        onClick={(e) => handleConnectorClick(e)}
+      ></span>
+      <Component />
+      <span
+        className="conector__line conector__line--right"
+        onClick={(e) => handleConnectorClick(e)}
+      ></span>
     </div>
   );
 }
