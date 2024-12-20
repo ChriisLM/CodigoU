df14196dbafb6b91d6ae440e576e1bb096131e90
ChriisLM <christianlezcano878@gmail.com>
2024-12-19 21:58:31 -0500

    feat: :sparkles: Add route of new page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { SimuladorCicuitos } from './pages/SimuladorCircuitos.tsx'
+import { CalculadorResistenica } from './pages/CalculadoraResistencia.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<App />}/>
         <Route path='/SimuladorCircuitos' element={<SimuladorCicuitos />}/>
+        <Route path='/CalculadoraResistencia' element={<CalculadorResistenica />}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
