d40093a204793769f2ea3674d980186414440d46
ChriisLM <christianlezcano878@gmail.com>
2024-12-19 21:56:18 -0500

    feat: :sparkles: Add new page of resistance calculator

---
+++ src/pages/CalculadoraResistencia.tsx
@@ -0,0 +1,14 @@
+import { Header } from "../components/Header";
+import { ProjectInfo } from "../components/ProjectInfo";
+
+
+export function CalculadorResistenica () {
+  return (
+    <>
+      <Header/>
+      <main>
+        <ProjectInfo id="CE-2"/>
+      </main>
+    </>
+  );
+};
