db41f0bd20a6d766197c024743d29d0692218baa
ChriisLM <christianlezcano878@gmail.com>
2024-12-24 21:10:16 -0500

    feat: :sparkles: Add functionality to add input

--- src/components/CalculadoraResistencia/ResistorCalculatorLayout.tsx
+++ src/components/CalculadoraResistencia/ResistorCalculatorLayout.tsx
@@ -1,23 +1,47 @@
-import "../../assets/css/CalculadoraResistencia/ResistorCalculatorLayout.css"
+import { useState } from "react";
+import "../../assets/css/CalculadoraResistencia/ResistorCalculatorLayout.css";
 import { ResistorInput } from "./ResistorInput";
 
 interface Props {
   typeCalculator: string;
 }
 
-export function ResistorCalculatorLayout ({typeCalculator}: Props) {
+export function ResistorCalculatorLayout({ typeCalculator }: Props) {
+  const resistorInputDefault = [
+    <ResistorInput resistencia="R1" deleteInputButton={false} />,
+    <ResistorInput resistencia="R2" deleteInputButton={false} />,
+  ];
+  const [resistors, setResistors] =
+    useState<JSX.Element[]>(resistorInputDefault);
+
+  const addResistorInput = () => {
+    const newResistorIndex = resistors.length + 1;
+    const newResistor = (
+      <ResistorInput
+        key={`R${newResistorIndex}`}
+        resistencia={`R${newResistorIndex}`}
+        deleteInputButton={true}
+        deleteInput={deleteResistorInput}
+      />
+    );
+    setResistors((prev) => [...prev, newResistor]);
+  };
+
+  const deleteResistorInput = (resistenciaRef: string) => {
+    setResistors((prev) =>
+      prev.filter((resistor) => resistor.props.resistencia !== resistenciaRef)
+    );
+  };
+
   return (
     <section>
       <aside>
-        <div>
-          <ResistorInput resistencia="R1" deleteInput={false}/>
-          <ResistorInput resistencia="R2" deleteInput={false}/>
-        </div>
-        <button>+</button>
+        <div>{resistors}</div>
+        <button onClick={addResistorInput}>+</button>
       </aside>
       <div>
         <span>{typeCalculator}</span>
       </div>
     </section>
   );
-};
+}
