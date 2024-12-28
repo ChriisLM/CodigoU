c13b023746c036529021eab834f8289384b38f78
ChriisLM <christianlezcano878@gmail.com>
2024-12-27 20:23:48 -0500

    feat: :sparkles: Add functionality recept input value

--- src/components/CalculadoraResistencia/ResistorInput.tsx
+++ src/components/CalculadoraResistencia/ResistorInput.tsx
@@ -1,23 +1,55 @@
-import "../../assets/css/CalculadoraResistencia/ResistorInput.css"
+import "../../assets/css/CalculadoraResistencia/ResistorInput.css";
 import { RemoveIcon } from "../../utils/Icons";
 
 interface Props {
   resistencia: string;
   deleteInputButton: boolean;
-  deleteInput?: (resistenciaRef: string) => void
+  deleteInput?: (resistenciaRef: string) => void;
+  onChange: (resistencia: string, value: number, format: string) => void;
 }
 
-export function ResistorInput ({resistencia,deleteInputButton,deleteInput}: Props) {
+export function ResistorInput({
+  resistencia,
+  deleteInputButton,
+  deleteInput,
+  onChange,
+}: Props) {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value) || 0;
+    const format = (e.target.nextSibling as HTMLSelectElement).value;
+    onChange(resistencia, value, format);
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value =
+      parseFloat((e.target.previousSibling as HTMLInputElement).value) || 0;
+    const format = e.target.value;
+    onChange(resistencia, value, format);
+  };
 
   return (
     <div className="resistorInput">
       <span className="resistorInput__title">{resistencia}</span>
       <div className="resistorInput__deleteSection">
-        {deleteInputButton && <RemoveIcon className="resistorInput__button-remove" onClick={() => deleteInput?.(resistencia)}/>}
+        {deleteInputButton && (
+          <RemoveIcon
+            className="resistorInput__button-remove"
+            onClick={() => deleteInput?.(resistencia)}
+          />
+        )}
       </div>
       <div className="resistorInput__inputSection">
-        <input type="number" className="resistorInput__input"/>
-        <select name="resistor" id="resistor" className="resistorInput__selector">
+        <input
+          type="number"
+          className="resistorInput__input"
+          onChange={handleInputChange}
+        />
+        <select
+          name="resistor"
+          id="resistor"
+          className="resistorInput__selector"
+          onChange={handleSelectChange}
+        >
           <option value="Ω">Ω</option>
           <option value="kΩ">kΩ</option>
           <option value="MΩ">MΩ</option>
@@ -25,4 +57,4 @@ export function ResistorInput ({resistencia,deleteInputButton,deleteInput}: Prop
       </div>
     </div>
   );
-};
+}
